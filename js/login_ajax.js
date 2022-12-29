a213371ccdbbdf28a136e67f28d133efc418308b
lr580 <2277379149@qq.com>
2022-12-29 15:33:32 +0800

    实现了后台登出

--- js/login_ajax.js
+++ js/login_ajax.js
@@ -1,43 +1,58 @@
 //@ sourceURL=login_ajax.js
-$(function(){
-	
-	//给form表单添加submit事件
-	$("form").submit(function(){
-		return login();
-	});
-	
-});
-function login(){
-	//获取页面数据
-	var userName=$("form input[name=username]").val();
-	var userPassword=$("form input[name=password]").val();
-	if(userName==""){
-		$("form table tr:eq(0) td span").html("用户名不能为空");
-		return false;
-	}
-	if(userPassword==""){
-		$("form table tr:eq(1) td span").html("密码不能为空");
-		return false;
-	}
+var dest;
+$(function() {
+    let url = window.location.href;
+    // let searcher = new URLSearchParams(url);
+    // console.log(url);
+    // let mode = searcher.get('mode');
+    // console.log(mode);
+    let res = url.match(/mode=(\d+)/);
+    if (res == null) { //前端登录
+        dest = "index.html";
+        $('#mtitle').html('欢迎登录EasyMall');
+    } else { //后台
+        dest = "manage.html";
+        $('#mtitle').html('欢迎登录EasyMall后台');
+    }
 
-	$.ajax({
-		url:"http://www.easymall.com/user/login",
-		type:"get",
-		data:{"userName":userName,"userPassword":userPassword},
-		dataType:"json",
-		success:function(result){
-			//result是服务端返回的数据
-			if(result.status==200){
-				//window.location.href="index.html";
-				window.location.href="index.html";
-			}else{
-				alert("登录失败");
-			}
-		},
-		error:function(){
-			alert("请求失败!");
-		}
-	});
-	
-	return false;
+    //给form表单添加submit事件
+    $("form").submit(function() {
+        return login();
+    });
+
+});
+
+function login() {
+    //获取页面数据
+    var userName = $("form input[name=username]").val();
+    var userPassword = $("form input[name=password]").val();
+    if (userName == "") {
+        $("form table tr:eq(0) td span").html("用户名不能为空");
+        return false;
+    }
+    if (userPassword == "") {
+        $("form table tr:eq(1) td span").html("密码不能为空");
+        return false;
+    }
+
+    $.ajax({
+        url: "http://www.easymall.com/user/login",
+        type: "get",
+        data: { "userName": userName, "userPassword": userPassword },
+        dataType: "json",
+        success: function(result) {
+            //result是服务端返回的数据
+            if (result.status == 200) {
+                //window.location.href="index.html";
+                window.location.href = dest;
+            } else {
+                alert("登录失败");
+            }
+        },
+        error: function() {
+            alert("请求失败!");
+        }
+    });
+
+    return false;
 }
--- js/logout.js
+++ js/logout.js
@@ -1,19 +1,23 @@
-function logout(){
+function logout(param) {
+    let dest = "http://www.easymall.com";
+    if (param) {
+        dest += "?mode=1";
+    }
     $.ajax({
-       url:"http://www.easymall.com/user/logout",
-       dataType:"json",
-       type:"GET",
-       success:function (data) {
-           if(data.status==200){
-               window.location.href="http://www.easymall.com/";
-               return;
-           }else{
-               alert("登出失败");
-               return;
-           }
-       },
-       erro:function () {
-           alert("登出请求发送失败");
-       }
+        url: "http://www.easymall.com/user/logout",
+        dataType: "json",
+        type: "GET",
+        success: function(data) {
+            if (data.status == 200) {
+                window.location.href = dest;
+                return;
+            } else {
+                alert("登出失败");
+                return;
+            }
+        },
+        erro: function() {
+            alert("登出请求发送失败");
+        }
     });
 }
